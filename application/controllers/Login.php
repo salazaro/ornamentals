184b8ac9a4e54a44c07431650cb339b99ab589f4
salazaro <salazaro@hotmail.com>
2017-02-11 11:56:24 -0600

    Actualizando rutas relativas con helper url

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('login');
+		$this->load->view('Login/login');
 	}
 }
